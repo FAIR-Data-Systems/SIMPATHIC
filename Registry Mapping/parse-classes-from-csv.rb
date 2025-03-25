5cd8513b3a2fd1fff0a398023683645e865f243f
Mark Wilkinson <markw@illuminae.com>
2025-03-25 14:46:24 +0100

    use erb templates. Add description

--- Registry Mapping/parse-classes-from-csv.rb
+++ Registry Mapping/parse-classes-from-csv.rb
@@ -1,5 +1,6 @@
 
 require 'csv'
+require 'erb'
 
 # Step 1: Generate the unique identifiers
 start_num = 12
@@ -20,67 +21,78 @@ rescue CSV::MalformedCSVError => e
   exit
 end
 
+
+
+classs = File.read "./templates/_toplevel.erb"
+header = File.read "./templates/_header.erb"
+footer = File.read "./templates/_footer.erb"
+@prefix = "https://w3id.org/nmd-domain#"
+
 top = {}
 f = File.open("test.owl", "w")
 
+owl = ERB.new(header).result
+f.write owl + "\n\n"
+
+
+# top categories come from first column
 first_column_values.each do |label|
   next unless label
-  next if top.keys.include? label
-  funcid = identifiers.shift
-  top[label] = funcid
-  a = %Q{<owl:Class rdf:about="https://w3id.org/nmd-domain##{funcid}">
-<rdfs:subClassOf rdf:resource="http://purl.obolibrary.org/obo/NCIT_C20993"/>
-<rdfs:label xml:lang="en">#{label}</rdfs:label>
-</owl:Class>\n\n}
-
-  f.write a + "\n"
-
+  next if top.keys.include? label  # already seen?
+  @funcid = identifiers.shift  # get the next id from the stack
+  top[label] = @funcid
+  @parent = "http://purl.obolibrary.org/obo/NCIT_C20993"
+  @classlabel = label
+  # use ERB template
+  owl = ERB.new(classs).result
+  f.write owl + "\n\n"
 end
 
+# now do the individual repos
 CSV.foreach(input_file, headers: true) do |row| # row[0] }
   next unless row[0]
-  parentid = top[row[0]]
-  thisid = identifiers.shift
-  enmdlabel = row[1]; enmddef = row[2]
-  dmslabel = row[3]; dmsdef = row[4]
-  enddm1label = row[5]; enddm1def = row[6]
-  myodraftlabel = row[7]; myodraftdef = row[8]
-  hpolabel = row[11]; hpo = row[12]
+  @parent = "#{@prefix}#{top[row[0]]}"
+  @funcid = identifiers.shift
+  @enmdlabel = row[1]; @enmddef = row[2]
+  @dmslabel = row[3]; @dmsdef = row[4]
+  @enddm1label = row[5]; @enddm1def = row[6]
+  @myodraftlabel = row[7]; @myodraftdef = row[8]
+  @hpolabel = row[11]; @hpo = row[12]
 
-  label = ""
-  label = hpolabel if  hpolabel
-  label = dmslabel if  dmslabel
-  label = enmdlabel if  enmdlabel
-  label = myodraftlabel if myodraftlabel
-  label = enddm1label if enddm1label
-  abort "label empty" unless label
-  abort "label empty" if label.empty?
+  @label = ""
+  @label = @hpolabel if  @hpolabel
+  @label = @dmslabel if  @dmslabel
+  @label = @enmdlabel if  @enmdlabel
+  @label = @myodraftlabel if @myodraftlabel
+  @label = @enddm1label if @enddm1label
+  abort "label empty" unless @label
+  abort "label empty" if @label.empty?
+  @classlabel = @label
 
-  description = ""
-  description = description + "EURO-NMD: " + enmddef + "; " if enmddef
-  description = description + "DM-Scope: " + dmsdef + "; " if dmsdef
-  description = description + "END-DM1: " + enddm1def + "; " if enddm1def
-  description = description + "Myodraft: " + myodraftdef + "; " if myodraftdef
-  description = description + "HPO: " + hpo + " " + hpolabel + "; " if hpo
-  abort "description empty" unless description
+  @description = ""
+  @description = @description + "EURO-NMD: " + @enmddef + "; " if @enmddef
+  @description = @description + "DM-Scope: " + @dmsdef + "; " if @dmsdef
+  @description = @description + "END-DM1: " + @enddm1def + "; " if @enddm1def
+  @description = @description + "Myodraft: " + @myodraftdef + "; " if @myodraftdef
+  @description = @description + "HPO: " + @hpo + " " + @hpolabel + "; " if @hpo
+  abort "description empty" if  @description.empty?
 
 
-  metadata = ""
-  metadata = metadata + "<dc:identifier>Myodraft: #{myodraftlabel}</dc:identifier>\n"  if myodraftlabel
-  metadata = metadata + "<dc:identifier>END-DM1: #{enddm1label}</dc:identifier>\n"  if enddm1label
-  metadata = metadata + "<dc:identifier>DM-Scope: #{dmslabel}</dc:identifier>\n"  if dmslabel
-  metadata = metadata + "<dc:identifier>EURO-NMD: #{enmdlabel}</dc:identifier>\n"  if enmdlabel
-  metadata = metadata + "<dc:identifier>HPO: #{hpo}</dc:identifier>\n"  if hpo
+  # @metadata = ""
+  # @metadata = @metadata + "Myodraft: #{@myodraftlabel}</dc:identifier>\n"  if @myodraftlabel
+  # @metadata = @metadata + "END-DM1: #{@enddm1label}</dc:identifier>\n"  if @enddm1label
+  # @metadata = @metadata + "DM-Scope: #{@dmslabel}</dc:identifier>\n"  if @dmslabel
+  # @metadata = @metadata + "EURO-NMD: #{@enmdlabel}</dc:identifier>\n"  if @enmdlabel
+  # @metadata = @metadata + "<dc:identifier>HPO: #{@hpo}</dc:identifier>\n"  if @hpo
   
-  a = %Q{<owl:Class rdf:about="https://w3id.org/nmd-domain##{thisid}">
-<rdfs:subClassOf rdf:resource="https://w3id.org/nmd-domain##{parentid}"/>
-<rdfs:label>#{label}</rdfs:label>
-#{metadata}
-</owl:Class>\n\n}
-
-  f.write a + "\n"
-
-
+  # use ERB template
+  owl = ERB.new(classs).result
+  f.write owl + "\n\n"
 end
 
+owl = ERB.new(footer).result
+f.write owl + "\n\n"
+
 puts "last identifier", identifiers.shift
+
+
