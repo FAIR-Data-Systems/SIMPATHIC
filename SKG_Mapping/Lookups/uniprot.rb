33103a9ba1dc4e55b8bacb9343e42fcc057a90cf
Mark Wilkinson <markw@illuminae.com>
2025-05-18 13:31:09 +0200

    disease_gene.rb takes 12 hours

--- SKG_Mapping/Lookups/uniprot.rb
+++ SKG_Mapping/Lookups/uniprot.rb
@@ -18,28 +18,30 @@ PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 PREFIX up: <http://purl.uniprot.org/core/>
 #SELECT ?protein ?fullName ?prefLabel ?recommended ?gene
-SELECT distinct ?protein ?tax ?pref ?alt ?recommended_full
+SELECT distinct ?ensgene ?protein ?tax ?pref ?alt ?recommended_full
 WHERE
 {
+        VALUES ?ensgene {|||ENSGENE|||}
         ?protein a up:Protein .
+        ?protein rdfs:seeAlso ?ensgene .
+
         ?protein up:reviewed true .
 #  		?protein rdfs:seeAlso ?other .
 #  		?other rdfs:comment ?alt_name .
   		?protein up:organism ?tax .
   OPTIONAL {?protein up:recommendedName ?rname . ?rname up:fullName ?recommended_full }
-		?protein rdfs:seeAlso |||ENSG||| .
   OPTIONAL {  		?protein skos:prefLabel ?pref }
   OPTIONAL {		?protein skos:altLabel ?alt }
-} LIMIT 10"
+}"
 
   def initialize()
   end
 
   def search_by_ensgene(ensg:)
     sparql = SPARQL::Client.new("https://sparql.uniprot.org/sparql/")
-    unless ensg =~ /proteinatlas/
-      # ensg = "<http://www.proteinatlas.org/#{ensg}>"
-      ensg = "<http://purl.uniprot.org/opentargets/#{ensg}>" # open targets is more reliable?
+    ensgenes = []
+    ensg.each do |id|
+      ensgenes << "<http://purl.uniprot.org/opentargets/#{id}>" # open targets is more reliable?
     end
   
     # warn "gene name #{ensg}"
@@ -47,7 +49,7 @@ WHERE
     retry_attempts = 1
     begin
       # warn ENSGQUERY.gsub("|||ENSG|||", ensg)
-      result = sparql.query(ENSGQUERY.gsub("|||ENSG|||", ensg),
+      result = sparql.query(ENSGQUERY.gsub("|||ENSGENE|||", ensgenes.join(" ")),
       read_timeout: 300, # 5 minutes
       open_timeout: 60   # 1 minute
       )
@@ -61,11 +63,11 @@ WHERE
         abort
       end
     end
-    if result.first
-      # SELECT distinct ?protein ?tax ?pref ?alt ?recommended_full
+    parsed = []
 
-      res = result.first  # first to get the recommended label
-      [res[:protein].to_s, res[:tax].to_s, res[:recommended_full].to_s]
+    result.each do |res|  # first to get the recommended label
+      parsed << [res[:ensgene].to_s, res[:protein].to_s, res[:tax].to_s, res[:recommended_full].to_s]
     end
+    parsed
   end
 end
